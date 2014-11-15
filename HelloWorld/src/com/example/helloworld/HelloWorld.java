f87f399ef425d77f05a89bb3b43cc7f084d0af17
liuzhen <445223894@qq.com>
2014-11-15 22:42:55 +0800

    my first java project

---
+++ HelloWorld/src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by arolla on 14-11-15.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
